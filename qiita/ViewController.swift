c995bc01c3cf58dcb93fbcbd3c6974e827ca887b
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-04-18 19:24:47 +0900

    Initial Commit

---
+++ qiita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiita
+//
+//  Created by 安部学 on 2020/04/18.
+//  Copyright © 2020 Manabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
